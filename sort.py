804f947796d740983329cf2fdec695017e9df9a5
PetoKrisa <petokrisa2006@gmail.com>
2023-10-05 18:57:10 +0200

    program done

--- sort.py
+++ sort.py
@@ -1,44 +1,75 @@
 import turtle as tutel
-import imp
 import random
+import time
+from time import sleep
+
 
 screen = tutel.Screen()
 screen.delay(0)
-screen.setup(500,350,startx=1,starty=1)
+screen.setup(500,350)
 screen.bgcolor("black")
-screen.title("Sorting Visualiser")
+screen.title("Sorting Visualiser by Kristóf Pető")
 tutel.color("white")
 tutel.hideturtle()
 
+SPEED = 0.1
+LENGTH = 50
+ALGORITHM = "bubblesort"
+FUNC = f"{ALGORITHM}(randomList({LENGTH}))"
 
-def draw(lista, highlight):
+def draw(lista, highlight=-2, finish=False):
     screen.tracer(0)
+
+    tutel.clear()
     tutel.penup()
-    tutel.goto((-240,-160))
+    tutel.goto(( ((screen.window_width()-30)/2)*-1, ((screen.window_height()-30)/2)*-1))
     for i in range(len(lista)):
+        wUnit = ((screen.window_width()-30)/len(lista))
+        hUnit =  ((screen.window_height()-100)/max(lista))
+        tutel.forward(wUnit/2)
         tutel.pendown()
         tutel.left(90)
-        tutel.width(10)
-        if i==highlight:
+        tutel.pensize(wUnit)
+        if (i==highlight or i==highlight+1) or (finish and i<=highlight):
             tutel.color("lime")
-        tutel.forward(lista[i]*5)
+
+        tutel.forward(lista[i]*hUnit)
         tutel.color("white")
+
         tutel.penup()
-        tutel.goto((tutel.xcor(),-160))
+        tutel.goto((tutel.xcor(), ((screen.window_height()-30)/2)*-1))
         tutel.right(90)
-        tutel.forward(12)
+        tutel.forward(wUnit/2)
     screen.update()
 
-draw([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15], 0)
+def sortFinisher(lista):
+    for i in range(len(lista)):
+        sleep(SPEED)
+        draw(lista,highlight=i,finish=True)
 
 def randomList(length) -> list:
-    lista = range(1,length)
-    return random.shuffle(lista)
+    lista = list(range(1,length+1))
+    random.shuffle(lista)
+    return lista
 
-def bubbleSort(lista):
+
+#algorithms
+def bubblesort(lista):
+    draw(lista)
+    sleep(1)
     for i in range(len(lista)):
-        #upload to git
+            for x in range(len(lista)):
+                if x+1 < len(lista):
+                    if lista[x] >= lista[x+1]:
+                        sleep(SPEED)
+                        draw(lista, highlight=x)
+                        num = lista[x]
+                        lista.pop(x)
+                        lista.insert(x+1,num)
+    sortFinisher(lista)
 
-bubbleSort([5,3,7,4,2,1,6])
+
+
+eval(FUNC)
 
 tutel.done()
